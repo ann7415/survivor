e4aec80c0962635d9efc6123bcc238ad29b5976e
Robin Schuffenecker <robin.schuffenecker@epitech.eu>
2025-09-02 17:02:54 +0200

    feat:(banner) add banner for startup or profile

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { Header } from './Header/header.component';
 import { FooterComponent } from './Footer/footer';
+import { BannerComponent } from './Components/Card/banner';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     styleUrls: ['./app.component.css'],
     template: `<app-header></app-header>
+               <app-banner title="Prout" text="Bienvenue à notre incubateur" image="https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg" buttonText="Nous contacter"></app-banner>
                <app-footer></app-footer>`,
-    imports: [Header, FooterComponent],
+    imports: [Header, BannerComponent, FooterComponent],
 })
 export class App {}
