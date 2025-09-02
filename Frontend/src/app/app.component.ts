41586c16928d1c4f6df845bed2fbe84ee113eab2
BERARD Romain <romain.berard@epitech.eu>
2025-09-03 01:11:23 +0200

    feat: Implement News component with styles and template

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Header } from './Header/header.component';
 import { ProjectComponent } from './Components/Project Card/project.component';
+import { NewsComponent } from './Components/News Card/news.component';
 
 @Component({
     selector: 'app-root',
@@ -9,7 +10,8 @@ import { ProjectComponent } from './Components/Project Card/project.component';
     template: `
         <app-header></app-header>
         <app-project src="https://angular.io/assets/images/logos/angular/angular.png" alt="Angular Logo" customClass="image-class" companyName="Google" projectName="Angular" description="A platform for building mobile and desktop web applications.A platform for building mobile and desktop web applications.A platform for building mobile and desktop web applications."></app-project>
-    `,
-    imports: [Header, ProjectComponent],
+        <app-news title="Angular News" imageUrl="https://angular.io/assets/images/logos/angular/angular.png"></app-news>
+        `,
+    imports: [Header, ProjectComponent, NewsComponent],
 })
 export class App {}
