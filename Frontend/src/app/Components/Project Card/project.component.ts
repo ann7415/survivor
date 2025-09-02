5f2182fb4e2fc9941e6707c3f08a57f89b0f4155
BERARD Romain <romain.berard@epitech.eu>
2025-09-02 16:37:37 +0200

    feat: Add Project Card component and associated styles
    - Created new Project Card component with HTML, CSS, and TypeScript files.
    - Implemented responsive design for the Project Card with a green background and structured layout.
    - Integrated Project Card into the main app component for demonstration.
    - Removed unnecessary SSR metadata and package files from the cache.
    - Added new News Card component files (HTML, CSS, TypeScript) for future development.

---
+++ Frontend/src/app/Components/Project Card/project.component.ts
@@ -0,0 +1,18 @@
+import { Component, input, Input } from '@angular/core';
+import { Image } from '../Image/image.component';
+
+@Component({
+    selector: 'app-project',
+    standalone: true,
+    templateUrl: './project.component.html',
+    styleUrls: ['./project.component.css'],
+    imports: [Image],
+})
+export class ProjectComponent {
+    @Input() alt?: string;
+    @Input() customClass?: string;
+    @Input({ required: true }) src!: string;
+    @Input({ required: true }) companyName!: string;
+    @Input({ required: true }) projectName!: string;
+    @Input({ required: true }) description!: string;
+}
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { Header } from './Header/header.component';
+import { ProjectComponent } from './Components/Project Card/project.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     styleUrls: ['./app.component.css'],
-    template: `<app-header></app-header>`,
-    imports: [Header],
+    template: `
+        <app-header></app-header>
+        <app-project src="https://angular.io/assets/images/logos/angular/angular.png" alt="Angular Logo" customClass="image-class" companyName="Google" projectName="Angular" description="A platform for building mobile and desktop web applications.A platform for building mobile and desktop web applications.A platform for building mobile and desktop web applications."></app-project>
+    `,
+    imports: [Header, ProjectComponent],
 })
 export class App {}
