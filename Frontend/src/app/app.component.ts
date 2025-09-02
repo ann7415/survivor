0515b7287116027c5a7a5beeb9ca938eed75c38c
BERARD Romain <romain.berard@epitech.eu>
2025-09-02 16:37:37 +0200

    feat: Add Project Card component and associated styles
    - Created new Project Card component with HTML, CSS, and TypeScript files.
    - Implemented responsive design for the Project Card with a green background and structured layout.
    - Integrated Project Card into the main app component for demonstration.
    - Removed unnecessary SSR metadata and package files from the cache.
    - Added new News Card component files (HTML, CSS, TypeScript) for future development.

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { Header } from './Header/header.component';
 import { FooterComponent } from './Footer/footer';
 import { BannerComponent } from './Components/Card/banner';
+import { ProjectComponent } from './Components/Project Card/project.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     styleUrls: ['./app.component.css'],
-    template: `<app-header></app-header>
-               <app-banner title="Prout" text="Bienvenue à notre incubateur" image="https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg" buttonText="Nous contacter"></app-banner>
-               <app-footer></app-footer>`,
-    imports: [Header, BannerComponent, FooterComponent],
+    template: `
+        <app-header></app-header>
+        <app-banner title="Prout" text="Bienvenue à notre incubateur" image="https://upload.wikimedia.org/wikipedia/commons/a/a6/Logo_NIKE.svg" buttonText="Nous contacter"></app-banner>
+        <app-project src="https://angular.io/assets/images/logos/angular/angular.png" alt="Angular Logo" customClass="image-class" companyName="Google" projectName="Angular" description="A platform for building mobile and desktop web applications.A platform for building mobile and desktop web applications.A platform for building mobile and desktop web applications."></app-project>
+        <app-footer></app-footer>
+    `,
+    imports: [Header, BannerComponent, FooterComponent, ProjectComponent],
 })
 export class App {}
