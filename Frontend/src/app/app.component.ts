6e346d131a40060a9c742d9eb80a8f37ea039ab9
BERARD Romain <romain.berard@epitech.eu>
2025-09-02 16:37:37 +0200

    feat: Add Project Card component and associated styles
    - Created new Project Card component with HTML, CSS, and TypeScript files.
    - Implemented responsive design for the Project Card with a green background and structured layout.
    - Integrated Project Card into the main app component for demonstration.
    - Removed unnecessary SSR metadata and package files from the cache.
    - Added new News Card component files (HTML, CSS, TypeScript) for future development.

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <router-outlet></router-outlet>
-  `,
-  imports: [RouterOutlet],
+    selector: 'app-root',
+    standalone: true,
+    styleUrls: ['./app.component.css'],
+    template: `<app-header></app-header>`,
+    imports: [RouterOutlet],
 })
 export class AppComponent {}
