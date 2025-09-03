074783c2000cb0aab273ffe344f30f6bfe11d83d
BERARD Romain <romain.berard@epitech.eu>
2025-09-03 11:15:55 +0200

    feat: Refactor ProjectsPage component structure and include ProjectComponent

--- Frontend/src/app/Page/Projects/projects.component.ts
+++ Frontend/src/app/Page/Projects/projects.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../Header/header.component';
 import { HeroComponent } from '../../Components/Hero/hero.component';
+import { ProjectComponent } from '../../Components/Project Card/project.component';
 
 @Component({
     selector: 'app-projects',
     standalone: true,
     styleUrls: ['./projects.component.css'],
     templateUrl: './projects.component.html',
-    imports: [HeaderComponent, HeroComponent],
+    imports: [HeaderComponent, HeroComponent, ProjectComponent],
 })
 export class ProjectsPage {}
