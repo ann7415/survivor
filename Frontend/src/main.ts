56f8bcc4bd576cec2fcc49a0c0c4a236013c37bd
BERARD Romain <romain.berard@epitech.eu>
2025-09-03 15:59:06 +0200

    feat: reorganize imports and enhance bootstrapApplication with HttpClient provider

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -7,18 +7,20 @@
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { HomePage } from './app/Page/Home/home.component';
+import { Routes, provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+
 import { UnknownPage } from './app/Page/Unknown/unknown.component';
+import { HomePage } from './app/Page/Home/home.component';
 import { ProjectsPage } from './app/Page/Projects/projects.component';
 import { NewsPage } from './app/Page/News/news.component';
-import { Routes, provideRouter } from '@angular/router';
 
 const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePage },
     { path: 'projects', component: ProjectsPage },
-    { path: 'news', component: NewsPage },  
+    { path: 'news', component: NewsPage },
     { path: '**', component: UnknownPage },
 ];
 
-bootstrapApplication(AppComponent, { providers: [provideRouter(routes)] }).catch(err => console.error(err));
+bootstrapApplication(AppComponent, { providers: [provideRouter(routes), provideHttpClient()] }).catch(err => console.error(err));
