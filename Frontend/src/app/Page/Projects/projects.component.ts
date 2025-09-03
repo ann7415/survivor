33d488befd0470d42d9741de8efae652a1d75c79
BERARD Romain <romain.berard@epitech.eu>
2025-09-03 11:45:08 +0200

    feat: Add Footer component to ProjectsPage and update imports

--- Frontend/src/app/Page/Projects/projects.component.ts
+++ Frontend/src/app/Page/Projects/projects.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../Header/header.component';
 import { HeroComponent } from '../../Components/Hero/hero.component';
 import { ProjectComponent } from '../../Components/Project Card/project.component';
+import { FooterComponent } from '../../Footer/footer';
 
 @Component({
     selector: 'app-projects',
     standalone: true,
     styleUrls: ['./projects.component.css'],
     templateUrl: './projects.component.html',
-    imports: [HeaderComponent, HeroComponent, ProjectComponent],
+    imports: [HeaderComponent, HeroComponent, ProjectComponent, FooterComponent],
 })
 export class ProjectsPage {}
